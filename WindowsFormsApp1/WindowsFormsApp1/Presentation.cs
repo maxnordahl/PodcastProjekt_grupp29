07e0a06bf95443f6eabd66d603b3b6f08595a3bd
Max Nordahl <max.nordahl@hotmail.se>
2017-10-25 13:20:27 +0200

    fixat 3 arkitektur

--- WindowsFormsApp1/WindowsFormsApp1/Presentation.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Presentation.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Presentation : Form
     {
-        public Form1()
+        public Presentation()
         {
             InitializeComponent();
         }
