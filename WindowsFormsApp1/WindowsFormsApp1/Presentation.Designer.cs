3b357669861f3abfbcfaec5a7aaa1ba1b3446b18
Ida Holme <idaholme@hotmail.com>
2017-10-25 13:53:13 +0200

    GUI1

--- WindowsFormsApp1/WindowsFormsApp1/Presentation.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Presentation.Designer.cs
@@ -28,33 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnPren = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.txtBoxURL = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnPren
             // 
-            this.button1.Location = new System.Drawing.Point(216, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnPren.Location = new System.Drawing.Point(427, 55);
+            this.btnPren.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPren.Name = "btnPren";
+            this.btnPren.Size = new System.Drawing.Size(106, 20);
+            this.btnPren.TabIndex = 0;
+            this.btnPren.Text = "Prenumerera";
+            this.btnPren.UseVisualStyleBackColor = true;
+            this.btnPren.Click += new System.EventHandler(this.btnPren_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(75, 58);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(32, 13);
+            this.lblURL.TabIndex = 1;
+            this.lblURL.Text = "URL:";
+            // 
+            // txtBoxURL
+            // 
+            this.txtBoxURL.Location = new System.Drawing.Point(113, 55);
+            this.txtBoxURL.Name = "txtBoxURL";
+            this.txtBoxURL.Size = new System.Drawing.Size(293, 20);
+            this.txtBoxURL.TabIndex = 2;
             // 
             // Presentation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(660, 449);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(919, 517);
+            this.Controls.Add(this.txtBoxURL);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.btnPren);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Presentation";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPren;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.TextBox txtBoxURL;
     }
 }
 
