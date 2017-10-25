d951de9726bb1eccc1974b15b868ce240b0d424e
Max Nordahl <max.nordahl@hotmail.se>
2017-10-25 13:27:18 +0200

    button

--- WindowsFormsApp1/WindowsFormsApp1/Presentation.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Presentation.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Presentation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(660, 449);
-            this.Name = "Form1";
+            this.Controls.Add(this.button1);
+            this.Name = "Presentation";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
