5479022aeaff654a228fa0d920f722245bf008fe
Ida Holme <idaholme@hotmail.com>
2017-10-26 08:50:55 +0200

    category

---
+++ WindowsFormsApp1/Logic/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    class Category
+    {
+        public string CateName { get; set; }
+
+        private Category()
+        {
+            this.CateName = CateName;
+        }
+
+        public string getCateName()
+        {
+            return CateName;
+        }
+    }
+}
